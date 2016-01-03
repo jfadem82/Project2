89e93b023a3f784fecca8bb5098b2f5fb26ceb80
Josh <jfadem82@gmail.com>
2016-01-03 05:48:15 -0800

    Adds customer params, delete button, links to each customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,7 @@
 class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :edit, :update, :destroy]
   def index
-    @customers = Customer.all
+    @customers = Customer.all.order('id ASC')
   end
 
   def new
@@ -8,7 +9,7 @@ class CustomersController < ApplicationController
   end
 
   def create
-    @customer = Customer.new(params.require( :customer ).permit( :first_name, :last_name, :gender, :email ))
+    @customer = Customer.new(customer_params)
 
     if @customer.save
       redirect_to customers_path
@@ -18,14 +19,34 @@ class CustomersController < ApplicationController
   end
 
   def show
+    if @customer.products
+      @products = @customer.products
+    end
   end
 
   def edit
   end
 
   def update
+    if @customer.update_attributes(customer_params)
+      redirect_to customers_path(@customer)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @customer.destroy
+    redirect_to root_path
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :gender, :email)
+  end
+
+  def set_customer
+    @customer = Customer.find(params[:id])
+  end
+
 end
