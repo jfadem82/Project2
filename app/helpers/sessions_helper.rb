5c9c41b0f5e63898b02c8e8ea40a8653d870788a
Josh <jfadem82@gmail.com>
2016-01-03 21:19:57 -0800

    Added before_action :authorize, only: [:show], rearranged buttons, restricted access to users when not logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,10 @@
 module SessionsHelper
 	def current_customer
-		@current_customer ||= Customer.find_by_id(session[:customer_id])
+		@current_customer ||= Customer.find_by_id(session[:customer_id]) if session[:customer_id]
 	end
+
+	def logged_in?
+		!!current_customer
+	end
+
 end
